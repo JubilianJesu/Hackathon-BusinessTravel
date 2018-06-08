9fcc2996afa75ec41e96778b1a8d951158c3228d
Sathishkumar Boobalan <39888634+Sathishkumarboobalan@users.noreply.github.com>
2018-06-08 15:57:47 -0500

    Add files via upload

--- lib/events/events_view.dart
+++ lib/events/events_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import '../model/offers.dart';
 import '../scroller/OfferPage.dart';
+import 'package:http/http.dart' as http;
 
 class MyEvents extends StatefulWidget {
   static String tag="myevents-Page";
@@ -12,29 +13,40 @@ class MyEvents extends StatefulWidget {
   }
 }
 
+  // Future<String> getPnrDetails() async{
+  //   http.Response response = await http.get(
+  //     Uri.encodeFull("http://businesstravel20180603032656.azurewebsites.net/api/Values"),
+  //     headers:{
+  //       "Accept" : "application/json"
+  //     }
+  //   );
+  //   print(response.body);
+  //   return response.body;
+  // }
+
 class EventsScroll extends State<MyEvents> {
 
   static final offers offersdata = new offers(
    
     images: <image>[ 
       new image(
-        title: 'Jazz in the Garden Concert Series, June 08, 2018 From: 05:00 PM to 08:30 PM',
-        thumbnail: 'assets/Event_1.JPG',
+        title: 'Jazz in the Garden Concert Series, On June 08, 2018',
+        thumbnail: '../assets/Event_2.JPG',
         url: 'https://washington.org/event/jazz-garden-concert-series',
       ),
       new image(
         title: 'DC Jazz Festival, From June 8-17, 2018',
-        thumbnail: 'assets/Event_2.jpg',
+        thumbnail: '../assets/Event_2.jpg',
         url: 'https://washington.org/visit-dc/reasons-to-attend-dc-jazz-festival',
       ),
       new image(
-        title: 'Experience a celebration of creativity and compassion through interactive exhibits, From June 21-24, 2018',
-        thumbnail: 'assets/Event_3.jpg',
+        title: 'Experience a creativity, From June 21-24, 2018',
+        thumbnail: '../assets/Event_3.jpg',
         url: 'https://washington.org/visit-dc/discover-by-the-people-festival',
       ),
       new image(
-        title: 'Girlfriend by Matthew Sweet and Todd Almond, Signature Theatre, 4200 Campbell Ave From June 10, 2018',
-        thumbnail: 'assets/Event_4.jpg',
+        title: 'Girlfriend , Signature Theatre, From June 10, 2018',
+        thumbnail: '../assets/Event_4.jpg',
         url: 'https://washington.org/event/girlfriend-matthew-sweet-and-todd-almond',
       ),
     ],
