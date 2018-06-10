f5f12702e6ac2e70b59e24660b73ac5fac3f7309
Prakash <v033319@global.ual.com>
2018-06-09 22:36:54 -0500

    Jubi

--- lib/tagPage.dart
+++ lib/tagPage.dart
@@ -1,160 +1,186 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
 import 'events/events_view.dart';
 import 'Offers/Offers_view.dart';
+import 'order/FlightCommonComponent.dart';
+import 'order/tripSummary.dart';
 
 class TagPage extends StatefulWidget {
-  static String tag="Tag-Page";
+  static String tag = "Tag-Page";
   @override
- State<StatefulWidget> createState() {
+  State<StatefulWidget> createState() {
     return new TagPageState();
   }
- 
 }
-class TagPageState extends State<TagPage>{
-   
-  
- @override
+
+class TagPageState extends State<TagPage> {
+  @override
   Widget build(BuildContext context) {
-       final logoHeader = Hero(
-        tag: "tcsLogo",
-        child: CircleAvatar(backgroundColor: Colors.transparent, radius: 50.0, child: Image.asset('assets/logo.png'),
-        ),
-      );
-
-      final eventsButton = Padding(
-        padding: EdgeInsets.symmetric(vertical:16.0,),
-        child: Material(
-            borderRadius:  BorderRadius.circular(18.0),
-            shadowColor: Colors.lightBlueAccent.shade100,
-            elevation: 5.0,
-            child: MaterialButton(
-              minWidth: 50.0,
-              height: 42.0,
-              onPressed: (){
-                Navigator.of(context).pushNamed(MyEvents.tag);
-              },
-              color: Colors.lightBlueAccent,
-              child: Text('Events', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+              leading: IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: () {
+                  print('Menu button');
+                },
               ),
-        ),
+              title: new Text("TAG")),
+          body: _buildBody(context)),
+    );
+  }
 
-      );
-
-final destinationButton = Padding(
-        padding: EdgeInsets.symmetric(vertical:16.0,),
-        child: Material(
-            borderRadius:  BorderRadius.circular(18.0),
-            shadowColor: Colors.lightBlueAccent.shade100,
-            elevation: 5.0,
-            child: MaterialButton(
-              minWidth: 50.0,
-              height: 42.0,
-              onPressed: (){
-                Navigator.of(context).pushNamed(MyOffers.tag);
-              },
-              color: Colors.lightBlueAccent,
-              child: Text('Destinations', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
-              ),
-        ),
-
-      );
-
-final oneOrderButton = Padding(
-        padding: EdgeInsets.symmetric(vertical:16.0,),
-        child: Material(
-            borderRadius:  BorderRadius.circular(18.0),
-            shadowColor: Colors.lightBlueAccent.shade100,
-            elevation: 5.0,
-            child: MaterialButton(
-              minWidth: 50.0,
-              height: 42.0,
-              onPressed: (){},
-              color: Colors.lightBlueAccent,
-              child: Text('One Order', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
-              ),
-        ),
-
-      );
-
-final wineAndDineButton = Padding(
-        padding: EdgeInsets.symmetric(vertical:16.0,),
-        child: Material(
-            borderRadius:  BorderRadius.circular(18.0),
-            shadowColor: Colors.lightBlueAccent.shade100,
-            elevation: 5.0,
-            child: MaterialButton(
-              minWidth: 50.0,
-              height: 42.0,
-              onPressed: (){},
-              color: Colors.lightBlueAccent,
-              child: Text('Wine & Dine', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
-              ),
-        ),
-
-      );
-
-final blendButton = Padding(
-        padding: EdgeInsets.symmetric(vertical:16.0,),
-        child: Material(
-            borderRadius:  BorderRadius.circular(18.0),
-            shadowColor: Colors.lightBlueAccent.shade100,
-            elevation: 5.0,
-            child: MaterialButton(
-              minWidth: 50.0,
-              height: 42.0,
-              onPressed: (){},
-              color: Colors.lightBlueAccent,
-              child: Text('Blend', style: TextStyle(color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold)),
-              ),
-        ),
-
-      );
-
-    var drawer = new Drawer();
-    return new Scaffold( // 1
-    drawer: drawer,
-      appBar: new AppBar(
-      title: new Text("Business Travel"),
-      actions: <Widget>[
-      logoHeader
-    ],
+  Widget _buildBody(BuildContext context) {
+    return new Container(
+        child: new ListView(scrollDirection: Axis.vertical, children: <Widget>[
+      new Container(
+        //height: 400.0,
+        child: buildTravelCart(),
       ),
-    body: Center(
-      child: ListView(
-        shrinkWrap: true,
-        padding: EdgeInsets.only(left: 24.0, right:24.0),
+      new Container(
+        height: 400.0,
+        child: TheGridView().build(context),
+      ),
+    ]));
+  }
+
+  Widget buildTravelCart() {
+    return new ClipRRect(
+      borderRadius: new BorderRadius.circular(8.0),
+      child: new Stack(
         children: <Widget>[
-          eventsButton,
-          SizedBox(height: 20.0),
-          destinationButton,
-          SizedBox(height: 20.0),
-          oneOrderButton,
-          SizedBox(height: 20.0),
-          wineAndDineButton,
-          SizedBox(height: 20.0),
-          blendButton,
-          SizedBox(height: 5.0),
-           new ListTile(
-              leading: new Icon(Icons.airport_shuttle),
-              title: new Text('Gates'),
+          //new Image.asset('assets/washingtonmonument.jpg',fit: BoxFit.cover),
+          new Positioned(
+            child: buildTravelDetails(),
+          ),
+          new Container(
+            child: new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                new Container(
+                  margin: const EdgeInsets.only(top: 150.0, bottom: 10.0),
+                  child: new Text(
+                    "Flight Status",
+                    style: new TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                new Container(
+                  margin: const EdgeInsets.only(
+                      top: 150.0, right: 5.0, bottom: 10.0),
+                  child: new Text(
+                    "Ontime",
+                    style: new TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.green,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
             ),
-          new ListTile(
-              leading: new Icon(Icons.map),
-              title: new Text('Airport Map'),
-            ),
-           
-            new ListTile(
-              leading: new Icon(Icons.flight_takeoff),
-              title: new Text('Flight Status'),
-            ),
+          )
         ],
-      )
-    ),
+      ),
+    );
+  }
+
+  FlightCommonComponent component = new FlightCommonComponent();
+  TripSummaryState tripSummary = new TripSummaryState();
+  Widget buildTravelDetails() {
+    var flights = component.getDummySelectedFlights();
+    List<Widget> widTrips = new List<Widget>();
+    for (var i = 0; i < flights.length; i++) {
+      widTrips.add(tripSummary.buildTripForSummary(context, flights[i]));
+    }
+    return new Container(
+      child: new Column(
+        children: widTrips,
+      ),
     );
   }
 }
-void orderDetailsClick(BuildContext context){
-  Navigator.of(context).pushNamed('/OrderDetails');
+
+class TheGridView {
+  GestureDetector makeGridCell(BuildContext context,
+      String name, String title, String image, int notificationCount) {
+    return new GestureDetector(
+      onTap: () {
+        iconClick(context,name);
+      },
+      child: new Card(
+        elevation: 1.0,
+        child: new ClipRRect(
+          borderRadius: new BorderRadius.circular(8.0),
+          child: new Stack(
+            children: <Widget>[
+              new Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  mainAxisSize: MainAxisSize.min,
+                  verticalDirection: VerticalDirection.down,
+                  children: <Widget>[
+                    new Image.asset(
+                      "assets/$image",
+                      fit: BoxFit.cover,
+                      height: 120.0,
+                    ),
+                    Center(
+                        child: Text(
+                      "$title",
+                      style: new TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20.0),
+                    )),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  GridView build(BuildContext context) {
+    return GridView.count(
+        primary: true,
+        padding: EdgeInsets.all(4.0),
+        crossAxisCount: 2,
+        childAspectRatio: 1.0,
+        mainAxisSpacing: 1.0,
+        crossAxisSpacing: 1.0,
+        children: <Widget>[
+          makeGridCell(context,"AirportMap", "Airport Map", "airporttile.png", 5),
+          makeGridCell(context,"Weather", "Weather", "Weather.gif", 5),
+          makeGridCell(context,"Events", "Events", "Events.jpg", 3),
+          makeGridCell(context,"Local Destinations", "Locale", "Locale.png", 3),
+          makeGridCell(context,"Wine&Dine", "Wine&Dine", "Wine&Dine.gif", 8)
+        ]);
+  }
+}
+
+void iconClick(BuildContext context, String icon) {
+  switch (icon) {
+    case "AirportMap":
+      //Navigator.of(context).pushNamed(MyEvents.tag);
+      break;
+    case "Weather":
+      Navigator.of(context).pushNamed('/OrderDetails');
+      break;
+    case "Wine&Dine":
+      Navigator.of(context).pushNamed('/OrderDetails');
+      break;
+    case "Local Destinations":
+      Navigator.of(context).pushNamed('/OrderDetails');
+      break;
+    default:
+      Navigator.of(context).pushNamed(MyEvents.tag);
+  }
 }
